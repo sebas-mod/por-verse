d1b7b7ae19e0e8d8addb3ac015d5f89512734336
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-10-10 13:28:13 -0300

    Hecho por SoyMaycol

---
+++ plugins/_/_antifile.js
@@ -0,0 +1,18 @@
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isMods }) {
+    if (m.isBaileys && m.fromMe) return true;
+    if (isOwner || isAdmin || isMods) return true;
+    let chat = global.db.data.chats[m.chat];
+    if (!chat) return true;
+    if (!chat.antiFile || !isBotAdmin) return true;
+    if (m.mtype === "documentMessage") {
+        await conn.sendMessage(m.chat, {
+            delete: {
+                remoteJid: m.chat,
+                fromMe: false,
+                id: m.key.id,
+                participant: m.key.participant,
+            },
+        });
+    }
+    return true;
+}
