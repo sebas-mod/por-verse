41146d09b50445feb1028b473adb740362469a60
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-10-10 13:27:43 -0300

    Hecho por SoyMaycol

---
+++ plugins/owner/owner-sf.js
@@ -0,0 +1,16 @@
+import fs from 'fs'
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) return m.reply(`✧ Ingresa la Ruta y el nombre del Archivo junto al comando.`)
+try {
+if (!m.quoted.text) return m.reply(`✧ Responder al mensaje.`)
+let path = `${text}`
+await fs.writeFileSync(path, m.quoted.text)
+m.reply(`✧ Guardado en *${path}*.`)
+} catch {
+await m.reply(`✧ Responder al mensaje.`)
+}}
+handler.command = ["savefile", "sf", "savecmd"]
+handler.tags = ['owner']
+handler.help = ['savefile']
+handler.rowner = true
+export default handler
