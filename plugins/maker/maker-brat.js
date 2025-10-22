a71426c2eeaf0a0613ec9235d29d052f641ce24c
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-10-22 15:53:59 -0300

    Refactor maker-brat.js for improved readability

--- plugins/maker/maker-brat.js
+++ plugins/maker/maker-brat.js
@@ -1,39 +1,39 @@
 import fs from 'fs'
-  import WSF from "wa-sticker-formatter";
-var handler = async (m, {
-    conn, 
-    args, 
-    text, 
-    usedPrefix, 
-    command
-}) => {
-    let ps = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.quoted && m.quoted.caption ? m.quoted.caption : m.quoted && m.quoted.description ? m.quoted.description : ''
-    if (!ps) throw m.reply(`*• Ejemplo :* ${usedPrefix + command} *[texto]*`)
+import WSF from "wa-sticker-formatter"
 
-    let res = `https://mxmxk-helper.hf.space/brat?text=${ps}`
+var handler = async (m, { conn, args, text, usedPrefix, command }) => {
+    let ps = text 
+        ? text 
+        : m.quoted?.text 
+        || m.quoted?.caption 
+        || m.quoted?.description 
+        || ''
+    
+    if (!ps) throw m.reply(`*• Ejemplo:* ${usedPrefix + command} [texto]`)
+
+    let res = `https://mxmxk-helper.hf.space/brat?text=${encodeURIComponent(ps)}`
 
     try {
-async function sticker(img, url, packname, author, categories = [""]) {
+        async function sticker(url, packname, author, categories = [""]) {
+            const stickerMetadata = {
+                type: "full",
+                pack: packname,
+                author,
+                categories,
+            }
+            return await new WSF.Sticker(url, stickerMetadata).build()
+        }
 
-  const stickerMetadata = {
-    type: "full",
-    pack: packname,
-    author,
-    categories,
-  };
-  return await new WSF.Sticker(img ? img : url, stickerMetadata).build();
-}
-
-        var stikerp = await sticker(res, { ps })
-        await conn.sendFile(m.chat, stikerp, 'atmin.webp', '', m)
+        var stikerp = await sticker(res, "Luffy Bot 🏴‍☠️", "Luis Sebastián")
+        await conn.sendFile(m.chat, stikerp, 'brat.webp', '', m)
     } catch (e) {
-        console.log(e)
-        await m.reply(eror)
+        console.error(e)
+        await m.reply(String(e))
     }
 }
 
-handler.help = ['brat'];
-handler.tags = ['sticker'];
-handler.command = /^(brat)$/i;
+handler.help = ['brat']
+handler.tags = ['sticker']
+handler.command = /^(brat)$/i
 
-export default handler;
+export default handler
