15cf7e47a6e242eea2235baa918459bd8098a46e
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-11-04 13:41:38 -0300

    Update pokemon-equipo.js

--- plugins/rpg-pokemon/pokemon-equipo.js
+++ plugins/rpg-pokemon/pokemon-equipo.js
@@ -1,15 +1,15 @@
 import fs from 'fs'
-const path = './plugins/rpg-pokemon/database/usuarios.json'
+import { pathUsuarios } from './rpgConfig.js'  // Importamos la config global
 
 let handler = async (m) => {
-  let usuarios = JSON.parse(fs.readFileSync(path))
-  let user = usuarios[m.sender]
-  if (!user) return m.reply('⚠️ Usá .iniciar primero.')
-  if (user.equipo.length === 0) return m.reply('❌ No tenés Pokémon en tu equipo.')
+let usuarios = JSON.parse(fs.readFileSync(pathUsuarios))
+let user = usuarios[m.sender]
+if (!user) return m.reply('⚠️ Usá .iniciar primero.')
+if (user.equipo.length === 0) return m.reply('❌ No tenés Pokémon en tu equipo.')
 
-  let msg = '🎒 *Tu equipo Pokémon:*\n'
-  user.equipo.forEach((p, i) => msg += `\n${i + 1}. ${p.nombre} (Lv.${p.nivel}) ❤️${p.vida}`)
-  m.reply(msg)
+let msg = '🎒 *Tu equipo Pokémon:*\n'
+user.equipo.forEach((p, i) => msg += `\n${i + 1}. ${p.nombre} (Lv.${p.nivel}) ❤️${p.vida}`)
+m.reply(msg)
 }
 
 handler.help = ['equipo']
