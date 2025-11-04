7502ffba5b7e607002251796d39a880dd9905dda
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2025-11-04 13:56:07 -0300

    Update pokemon-renombrar.js

--- plugins/rpg-pokemon/pokemon-renombrar.js
+++ plugins/rpg-pokemon/pokemon-renombrar.js
@@ -1,20 +1,20 @@
 import fs from 'fs'
-const path = './plugins/rpg-pokemon/database/usuarios.json'
+import { pathUsuarios } from './rpgConfig.js'
 
 let handler = async (m, { args, usedPrefix, command }) => {
-  let usuarios = JSON.parse(fs.readFileSync(path))
-  let user = usuarios[m.sender]
-  if (!user) return m.reply(`⚠️ Usá ${usedPrefix}iniciar primero.`)
-  if (user.equipo.length === 0) return m.reply('❌ No tenés Pokémon.')
+let usuarios = JSON.parse(fs.readFileSync(pathUsuarios))
+let user = usuarios[m.sender]
+if (!user) return m.reply(`⚠️ Usá ${usedPrefix}iniciar primero.`)
+if (!user.equipo || user.equipo.length === 0) return m.reply('❌ No tenés Pokémon.')
 
-  let index = parseInt(args[0]) - 1
-  let nuevoNombre = args.slice(1).join(' ')
-  if (isNaN(index) || index < 0 || index >= user.equipo.length) return m.reply('❌ Posición inválida.')
-  if (!nuevoNombre) return m.reply(`💡 Usa: *${usedPrefix}${command} <posición> <nuevo nombre>*`)
+let index = parseInt(args[0]) - 1
+let nuevoNombre = args.slice(1).join(' ')
+if (isNaN(index) || index < 0 || index >= user.equipo.length) return m.reply('❌ Posición inválida.')
+if (!nuevoNombre) return m.reply(`💡 Usa: *${usedPrefix}${command} <posición> <nuevo nombre>*`)
 
-  user.equipo[index].nombre = nuevoNombre
-  fs.writeFileSync(path, JSON.stringify(usuarios, null, 2))
-  m.reply(`✅ Tu Pokémon ahora se llama *${nuevoNombre}*.`)
+user.equipo[index].nombre = nuevoNombre
+fs.writeFileSync(pathUsuarios, JSON.stringify(usuarios, null, 2))
+m.reply(`✅ Tu Pokémon ahora se llama *${nuevoNombre}*.`)
 }
 
 handler.help = ['renombrar <posición> <nombre>']
